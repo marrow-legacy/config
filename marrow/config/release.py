68b8a3555b8aec6395ff425b744e7418667a6fe7
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-10-14 16:42:33 -0700

    Rename in release information.

--- marrow/config/release.py
+++ marrow/config/release.py
@@ -3,15 +3,15 @@
 """Release information about Pulp."""
 
 
-name = "pulp.config"
+name = "marrow.config"
 version = "0.1.0"
 release = "0.1.0"
 
 summary = "Useful extensions to PyYAML for use as an application configuration system in Python 2.x and 3.x."
 description = """"""
 author = "Alice Bevan-McGregor"
-email = "alice+pulp@gothcandy.com"
-url = "http://github.com/pulp/pulp.config"
-download_url = "http://cheeseshop.python.org/pypi/pulp.config/"
+email = "alice+marrow@gothcandy.com"
+url = "http://github.com/pulp/marrow.config"
+download_url = "http://cheeseshop.python.org/pypi/marrow.config/"
 copyright = "2010, Alice Bevan-McGregor"
 license = "MIT"
